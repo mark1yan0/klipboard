30d784a6476fbf56de17c13cfc80a4a11a880b2d
mark1yan0 <markiyan.dev@gmail.com>
2025-04-25 23:31:47 +0200

    Initial Commit

---
+++ klipboard/klipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  klipboardApp.swift
+//  klipboard
+//
+//  Created by Markiyan Kmit on 25/04/25.
+//
+
+import SwiftUI
+
+@main
+struct klipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ klipboardTests/klipboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  klipboardTests.swift
+//  klipboardTests
+//
+//  Created by Markiyan Kmit on 25/04/25.
+//
+
+import Testing
+@testable import klipboard
+
+struct klipboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
